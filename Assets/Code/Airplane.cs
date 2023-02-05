389959c330a5f4c7e2c25efcaf8de5cbb8281a7a
Leo <77863733+leofox2021@users.noreply.github.com>
2023-02-06 02:07:28 +0300

    initial commit. PLANE MODEL WILL BE REPLACED SOON

---
+++ Assets/Code/Airplane.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airplane : MonoBehaviour {
+
+    public Rigidbody2D airplane; 
+    
+    // Start is called before the first frame update
+    public void Start() {
+        
+    }
+
+    // Update is called once per frame
+    public void Update() {
+        
+    }
+}
